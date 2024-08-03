76a3d5f76e7434061ba627a136095da2c81194d6
Aamir Ali <aa882755@gmail.com>
2024-08-03 18:07:15 +0530

    What Is An Entity Domain-Driven Design, Clean Architecture .NET 6

--- src/Gatherly/Gatherly.Domain/Entities/Invitation.cs
+++ src/Gatherly/Gatherly.Domain/Entities/Invitation.cs
@@ -1,17 +1,18 @@
-﻿namespace Gatherly.Domain.Entities;
+﻿using Gatherly.Domain.Enums;
+using Gatherly.Domain.Primitives;
 
-public class Invitation
+namespace Gatherly.Domain.Entities;
+
+public sealed class Invitation : Entity
 {
     internal Invitation(Guid id, Member member, Gathering gathering)
+        : base(id)
     {
-        Id = id;
         MemberId = member.Id;
         GatheringId = gathering.Id;
         Status = InvitationStatus.Pending;
         CreatedOnUtc = DateTime.UtcNow;
     }
-
-    public Guid Id { get; private set; }
 
     public Guid GatheringId { get; private set; }
 
