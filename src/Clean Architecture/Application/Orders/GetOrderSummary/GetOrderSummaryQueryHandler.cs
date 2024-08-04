edb9eb409d93e246f259bd6c148d3e9c6792fb0c
Aamir Ali <aa882755@gmail.com>
2024-08-04 09:37:34 +0530

    Deep Dive Into EF Core 8 Raw SQL Queries NEW FEATURE

--- src/Clean Architecture/Application/Orders/GetOrderSummary/GetOrderSummaryQueryHandler.cs
+++ src/Clean Architecture/Application/Orders/GetOrderSummary/GetOrderSummaryQueryHandler.cs
@@ -17,8 +17,11 @@ internal sealed class GetOrderSummaryQueryHandler
 
     public async Task<OrderSummary?> Handle(GetOrderSummaryQuery request, CancellationToken cancellationToken)
     {
-        return await _context.OrderSummaries
-            .AsNoTracking()
-            .FirstOrDefaultAsync(o => o.Id == request.OrderId, cancellationToken);
+        return await _context.Database
+            .SqlQuery<OrderSummary>($@"
+                SELECT *
+                FROM order_summaries
+                WHERE id = {request.OrderId}")
+            .FirstOrDefaultAsync(cancellationToken);
     }
 }
