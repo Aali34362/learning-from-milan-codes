5a7191156d4efa4133389bd3de39fb90b40bba79
Aamir Ali <aa882755@gmail.com>
2024-08-04 22:09:53 +0530

    How To Use Both EF Core and Dapper For CQRS Queries

--- src/running_tracker/src/Domain/Users/UserErrors.cs
+++ src/running_tracker/src/Domain/Users/UserErrors.cs
@@ -6,4 +6,7 @@ public static class UserErrors
 {
     public static Error NotFound(Guid userId) => new(
         "Users.NotFound", $"The user with the Id = '{userId}' was not found");
+
+    public static Error NotFoundByEmail(string email) => new(
+        "Users.NotFoundByEmail", $"The user with the Email = '{email}' was not found");
 }
