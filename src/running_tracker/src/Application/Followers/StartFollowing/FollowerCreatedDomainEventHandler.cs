e0027158f2b1a3e7bba4fc1ec0b4cb8bca9f3fa5
Aamir Ali <aa882755@gmail.com>
2024-08-04 22:15:32 +0530

    How To Make Your Application Queries Reusable

--- src/running_tracker/src/Application/Followers/StartFollowing/FollowerCreatedDomainEventHandler.cs
+++ src/running_tracker/src/Application/Followers/StartFollowing/FollowerCreatedDomainEventHandler.cs
@@ -1,6 +1,11 @@
-﻿using Application.Abstractions.Notifications;
+﻿using System.Data;
+using Application.Abstractions.Data;
+using Application.Abstractions.Notifications;
+using Application.Users;
 using Domain.Followers;
+using Domain.Users;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Followers.StartFollowing;
 
@@ -8,17 +13,25 @@ internal sealed class FollowerCreatedDomainEventHandler
     : INotificationHandler<FollowerCreatedDomainEvent>
 {
     private readonly INotificationService _notificationService;
+    private readonly IDbConnectionFactory _dbConnectionFactory;
 
-    public FollowerCreatedDomainEventHandler(INotificationService notificationService)
+    public FollowerCreatedDomainEventHandler(
+        INotificationService notificationService,
+        IDbConnectionFactory dbConnectionFactory)
     {
         _notificationService = notificationService;
+        _dbConnectionFactory = dbConnectionFactory;
     }
 
     public async Task Handle(FollowerCreatedDomainEvent notification, CancellationToken cancellationToken)
     {
+        using IDbConnection connection = _dbConnectionFactory.CreateOpenConnection();
+
+        UserDto user = await UserQueries.GetByIdAsync(notification.UserId, connection);
+
         await _notificationService.SendAsync(
             notification.FollowedId,
-            "You just got a new follower",
+            $"{user.Name} started following you",
             cancellationToken);
     }
 }
---
+++ src/running_tracker/src/Application/Users/UserQueries.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Dapper;
+
+namespace Application.Users;
+
+public static class UserQueries
+{
+    public static async Task<UserDto> GetByIdAsync(
+        Guid id,
+        IDbConnection connection)
+    {
+        return await connection.QuerySingleAsync<UserDto>(
+            """
+            SELECT Id, Name
+            FROM Users
+            WHERE Id = @UserId
+            """,
+            new { UserId = id });
+    }
+}
