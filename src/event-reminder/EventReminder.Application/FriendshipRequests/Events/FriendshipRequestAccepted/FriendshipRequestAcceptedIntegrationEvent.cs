aececcc93dad9a919a73d032e0985abbeb8086eb
Aamir Ali <aa882755@gmail.com>
2024-08-04 20:39:40 +0530

    Fix The Domain Layer Folder Structure With Vertical Slices

---
+++ src/event-reminder/EventReminder.Application/FriendshipRequests/Events/FriendshipRequestAccepted/FriendshipRequestAcceptedIntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using EventReminder.Application.Core.Abstractions.Messaging;
+using System;
+using Newtonsoft.Json;
+using EventReminder.Domain.Friendships.DomainEvents;
+
+namespace EventReminder.Application.FriendshipRequests.Events.FriendshipRequestAccepted
+{
+    /// <summary>
+    /// Represents the integration event that is raised when a friendship request is accepted.
+    /// </summary>
+    public sealed class FriendshipRequestAcceptedIntegrationEvent : IIntegrationEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FriendshipRequestAcceptedIntegrationEvent"/> class.
+        /// </summary>
+        /// <param name="friendshipRequestAcceptedDomainEvent">The friendship request accepted domain event.</param>
+        internal FriendshipRequestAcceptedIntegrationEvent(FriendshipRequestAcceptedDomainEvent friendshipRequestAcceptedDomainEvent) =>
+            FriendshipRequestId = friendshipRequestAcceptedDomainEvent.FriendshipRequest.Id;
+
+        [JsonConstructor]
+        private FriendshipRequestAcceptedIntegrationEvent(Guid friendshipRequestId) => FriendshipRequestId = friendshipRequestId;
+
+        /// <summary>
+        /// Gets the friendship request identifier.
+        /// </summary>
+        public Guid FriendshipRequestId { get; }
+    }
+}
